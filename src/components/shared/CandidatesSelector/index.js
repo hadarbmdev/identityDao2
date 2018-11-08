575787145a8d5c5a66179dd5174c8ba1faaa999f
Tal Batz <talbatz@Tals-MacBook-Pro.local>
2018-11-09 00:51:05 +0200

    buttons

--- src/components/shared/CandidatesSelector/index.js
+++ src/components/shared/CandidatesSelector/index.js
@@ -5,6 +5,10 @@ import SwipeableViews from 'react-swipeable-views';
 import { Typography } from '@material-ui/core';
 import { Grid } from '@material-ui/core';
 import { Paper } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import IconButton from '@material-ui/core/IconButton';
+
 
 
 const CandidatesSelector = (props) => {
@@ -32,10 +36,20 @@ const CandidatesSelector = (props) => {
       <Grid container spacing={24}>
 
         <Grid item xs={6} sm={3}>
-          <Paper>xs=6 sm=3</Paper>
+
+      <Button variant="extendedFab" aria-label="Delete" >
+        <Icon />
+        Extended
+      </Button>
+
         </Grid>
         <Grid item xs={6} sm={3}>
-          <Paper>xs=6 sm=3</Paper>
+
+      <Button variant="extendedFab" aria-label="Delete" >
+        <Icon />
+        Extended
+      </Button>
+
         </Grid>
       </Grid>
 
