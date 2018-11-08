890054207676be10e60d4faca0002610e8ea9baf
Tal Batz <talbatz@Tals-MacBook-Pro.local>
2018-11-08 19:19:49 +0200

    Social set (icons) initial

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Link, Redirect, Switch } from 'react-router-dom';
 import { Welcome } from '../../components/Welcome';
 import { Register } from '../../components/Register';
+import { Socialset } from '../../components/Socialset';
 
 export default function Main() {
   return (
@@ -10,6 +11,8 @@ export default function Main() {
         <Route exact path="/" component={Welcome} />
         <Route exact path="/welcome" component={Welcome} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/socialset" component={Socialset} />
+
     </div>
   );
 }
