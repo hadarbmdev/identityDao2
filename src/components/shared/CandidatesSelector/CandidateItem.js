b8bb864b807d840d59705c96f7b87eedee490416
Hadar Ben Maor <hadarbmdev@gmail.com>
2018-11-08 20:15:19 +0200

    swipe initial working

--- src/components/shared/CandidatesSelector/CandidateItem.js
+++ src/components/shared/CandidatesSelector/CandidateItem.js
@@ -1,7 +1,7 @@
 
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import Card from '@material-ui/core/Card';
+import Paper from '@material-ui/core/Paper';
 import CardActions from '@material-ui/core/CardActions';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
@@ -17,37 +17,21 @@ class CandidateItem extends Component {
   static propTypes = {
     id: PropTypes.string,
     photo: PropTypes.string,
-    firstname:PropTypes.string,
-    lastname:PropTypes.string,
+    firstname: PropTypes.string,
+    lastname: PropTypes.string,
   }
   render() {
 
-    const {id, firstname,lastname, photo} = this.props
+    const { key, firstname, lastname, photo } = this.props
     return (
-
-      <Card>
-      <CardActionArea>
-        <CardMedia
-          className = "cardPhoto"
-           image={photo}
-          title={firstname + " " + lastname}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-          {firstname + " " + lastname}
-          </Typography>
-        
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
+      <div>
+      
+          <img src={photo} />
+          <div>
+            {firstname + " " + lastname}
+          </div>
+       
+      </div>
     )
   }
 }
