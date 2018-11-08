e1d9f97b1064e469a966194131d954c83a81c530
Lior Orenbach <lioror@etoro.com>
2018-11-08 23:45:56 +0200

    added payment mock

--- src/components/Payment/index.js
+++ src/components/Payment/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom'
 import Button from '@material-ui/core/Button';
-import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
 import Paper from '@material-ui/core/Paper';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { Typography } from '@material-ui/core';
+import { Typography, IconButton, Grid, Toolbar, Avatar, Input, InputAdornment } from '@material-ui/core';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+
 
 
 class Payment extends React.Component {
@@ -15,20 +15,68 @@ class Payment extends React.Component {
         super(props);
     }
 
-
     render() {
         const registered = this.props.registered
 
         return (
-            <Paper>
-<Typography>hi</Typography>
-            </Paper>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center'
+            }}>
+                <Paper style={{
+                    width: '60vh',
+                    height: '80vh',
+                    marginTop: '2vh'
+                }}>
+                    <Toolbar>
+                        <IconButton color="inherit" aria-label="Menu">
+                            <ArrowBack />
+                        </IconButton>
+                    </Toolbar>
+                    <Grid
+                        container
+                        spacing={16}
+                        direction="column"
+                        justify="space-evenly"
+                        alignItems="center"
+                    >
+                        <Grid item>
+                            <Avatar
+                                alt="Adelle Charles"
+                                src="https://via.placeholder.com/200"
+                                style={{ width: '20vh', height: '20vh' }}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Typography variant='caption'>
+                                Vouch for Noam
+                    </Typography>
+                        </Grid>
+
+                        <Grid item>
+                            <Typography variant='h4'>
+                                How much Eth <br />
+                                Are you willing to Vouch
+                    </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Input
+                                placeholder='0.3'
+                                //value={this.state.weight}
+                                //onChange={this.handleChange('weight')}
+                                endAdornment={<InputAdornment position="end">ETH</InputAdornment>}
+                            />
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </div>
         )
     }
 }
 
 Payment.propTypes = {
     registered: PropTypes.string,
+    classes: PropTypes.object.isRequired,
 };
 
 function mapStateToProps(state) {
--- src/components/shared/CandidatesSelector/index.js
+++ src/components/shared/CandidatesSelector/index.js
@@ -43,7 +43,7 @@ const CandidatesSelector = (props) => {
   return (
     <SwipeableViews>
         {candidates}
-  </SwipeableViews>
+    </SwipeableViews>
     
     
   )
