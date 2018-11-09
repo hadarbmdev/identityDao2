d185e18a52679cf00723397a12e33ea5b7f27a45
Tal Batz <talbatz@Tals-MacBook-Pro.local>
2018-11-09 08:59:10 +0200

    new vouch/fake buttons on voucher

--- src/components/Voucher/index.js
+++ src/components/Voucher/index.js
@@ -73,17 +73,17 @@ class Voucher extends React.Component {
 
                         <Grid item xs={6} sm={6}>
 
-                            <Button aria-label="Delete" onClick={()=>this.Vouche()}>
-                                <img src="/buttonFake.png" />
-                            </Button>
+                            <a aria-label="Delete" onClick={()=>this.Vouche()} className="roundButton">
+                                <img src="/buttonFake.svg" />
+                            </a>
 
                         </Grid>
 
                         <Grid item xs={6} sm={6}>
 
-                            <Button aria-label="Delete" onClick={()=>this.Fake()} >
-                                <img src="/buttonVouch.png" />
-                            </Button>
+                            <a aria-label="Delete" onClick={()=>this.Fake()}  className="roundButton">
+                                <img src="/buttonVouch.svg" />
+                            </a>
 
                         </Grid>
                     </Grid>
