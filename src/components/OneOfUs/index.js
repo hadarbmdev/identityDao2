639e92b52878adeec1ff4d5e22416d1f23328d70
Lior Orenbach <lioror@etoro.com>
2018-11-09 05:06:46 +0200

    hhhhhh

--- src/components/OneOfUs/index.js
+++ src/components/OneOfUs/index.js
@@ -7,6 +7,9 @@ import PropTypes from 'prop-types';
 import Paper from '@material-ui/core/Paper';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import Mewe from './assets/mewe.png'
+import CenterImage from './assets/1_Visual.png'
+import Logo from './assets/GoodDollar_LOGO.png'
 
 
 class OneOfUs extends React.Component {
@@ -25,25 +28,47 @@ class OneOfUs extends React.Component {
                 <Grid container style={{ height: '100vh' }}
                 >
                     <Grid item xs={12}>
-                        <h1>One of us</h1>
-                        <h3>Identity made for you</h3>
+                        <img src={Mewe}
+                            style={{
+                                height: '15vh',
+                                width: '15vh',
+                                border: '2px solid white',
+                                borderRadius: '5px'
+                            }}
+                        />
+                        <h3 style={{ color: 'white' }}>Identity made for you</h3>
+                    </Grid>
+
+                    <Grid xs={12}>
+                        <img src={CenterImage} 
+                        style={{height:'40vh'}}
+                        />
                     </Grid>
 
                     <Grid item xs={12}>
                         <Grid container
                             direction={'column'}
                             alignItems={'center'}
+                            justify={'space-around'}
                             spacing={8}
                         >
-                            <Grid item xs={4}>
-                                <Button variant="contained"
+                            <Grid item>
+                                <Button variant='contained'
                                     onClick={this.handleClick}
-                                    color="primary"
+                                    color='primary'
                                 >vouch and earn
                                 </Button>
                             </Grid>
-                            <Grid item xs={4}>
-                                <Button variant="contained" color="primary">create id</Button>
+                            <Grid item >
+                                <Button variant='contained' color='secondary' disabled>create id</Button>
+                            </Grid>
+                            <Grid>
+                                <Grid container alignItems={'center'}>
+                                    <h5 style={{color:'white'}}>powered by</h5>
+                                    <img src={Logo} 
+                                    style={{height:'5vh'}}
+                                    />
+                                </Grid>
                             </Grid>
                         </Grid>
                     </Grid>
